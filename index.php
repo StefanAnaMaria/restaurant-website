f965139db92173e919d057812c13f1b9a3fadfed
StefanAnaMaria <139035929+StefanAnaMaria@users.noreply.github.com>
2025-03-09 14:55:24 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,58 +1,12 @@
-<?php
 
-session_start();
 
-if (isset($_SESSION["user_id"])) {
-    
-    $mysqli = require __DIR__ . "/database.php";
-    
-    $sql = "SELECT * FROM user
-            WHERE id = {$_SESSION["user_id"]}";
-            
-    $result = $mysqli->query($sql);
-    
-    $user = $result->fetch_assoc();
-}
+<?php include 'components/header.php'; ?>   
+<?php include 'components/navbar.php'; ?>   
+<div class="container mt-5">
+    <!-- Menu Header -->
+    <div class="text-center mb-5">
+        <H1>Du-te la </h1><a href="pages/home.php">Acasa</h1>
+</div>
+</div>
 
-?>
-<!-- <!DOCTYPE html>
-<html>
-<head>
-    <title>Home</title>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
-</head>
-<body> -->
-<?php include('components/header.php'); ?>   
-    <h1>Home</h1>
-    
-    <?php if (isset($user)): ?>
-        
-        <p>Hello <?= htmlspecialchars($user["name"]) ?></p>
-        
-        <p><a href="logout.php">Log out</a></p>
-        
-    <?php else: ?>
-        
-        <p><a href="login.php">Log in</a> or <a href="signup.html">sign up</a></p>
-        
-    <?php endif; ?>
-    
-
-    <!-- Bootstrap JS and dependencies -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
-    <!-- Icons (optional, if using Bootstrap Icons) -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
-</body>
-</html
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+<?php include 'components/footer.php'; ?> 
